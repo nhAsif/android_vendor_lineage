2ef077ad706e84c49dddcee2fd13d29ddfd19070
karthick111 <karthickrko61@gmail.com>
2022-01-23 11:29:30 +0530

    vendor: config: Update raven fingerprint
    Change-Id: Ic47cb800b67cb19509c568244e7a8266e0ec4364

--- config/common.mk
+++ config/common.mk
@@ -27,7 +27,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
-BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.211205.017/7955197:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
